f0da602f8136be63cf5c9682ce4fd1f2395c8d5c
Luxshan-Partheepan-ICD2O1-01 <luxshan.partheepan@student.tdsb.on.ca>
2024-11-05 19:50:08 -0500

    Update PP1_9.py

--- PP1_9.py
+++ PP1_9.py
@@ -1,31 +1,38 @@
 
 
 def q1(): 
-  #Write Assignment code here
+  print('"Hello World"')
+
 
 
 def q2(): 
-  #Write Assignment code here
+  word = input("Input a word: ")
+  print(word.lower())  
+  print(word.upper())  
+
 
 
 def q3(): 
-  #Write Assignment code here
+  words = input("Input a word that is at least 5 letters long: ")
+  print(word5[1:4])
 
 
 def q4(): 
-  #Write Assignment code here
-
+  letterwitho = input("Input a word: ")
+  print(letterwitho.find('o')
+  
 
 def q5(): 
-  #Write Assignment code here
+  longword = input("Input a word: ")
+  print(len(longword))
 
 
 
 #Do not alter the following code
 #Comment out the following code when running your tests
 
-q1()
-q2()
-q3()
-q4()
-q5()
+#q1()
+#q2()
+#q3()
+#q4()
+#q5()
